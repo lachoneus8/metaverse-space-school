d61227ffe43792533ad2b75a97f3406825ba1da0
lachoneus8 <2500010+lachoneus8@users.noreply.github.com>
2025-04-22 15:59:08 -0600

    Add indirection so we can load any world
    Adds the ability to set up a prefab that tracks the current world index of the planet we are going to visit, and records it between Unity scenes.

--- Assets/Scripts Main/DataDrivenWorldSetup.cs
+++ Assets/Scripts Main/DataDrivenWorldSetup.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class DataDrivenWorldSetup : MonoBehaviour
 {
-    public WorldData worldData;
+    public GameObject currentWorldIndexPrefab;
+
+    public List<WorldData> worldDataList;
 
     public TMP_Text titleText;
 
@@ -12,20 +15,41 @@ public class DataDrivenWorldSetup : MonoBehaviour
     public TMP_Text dayLengthText;
     public TMP_Text distanceFromSunText;
 
+    public GravitySetting gravitySetting;
+
     public MeshRenderer groundPlane;
+
+    private WorldData currentWorldData;
 
     void Start()
     {
-        titleText.text = "Welcome to " + worldData.worldName;
-
-        gravityText.text = "Gravity: " + worldData.gravityMPS2 + "m/s^2";
-        temperatureText.text = "Temperature: " + worldData.minTempF + "-" + worldData.maxTempF + " F";
-        dayLengthText.text = "Day length: " + worldData.dayNightRotationLength.ToString();
-        distanceFromSunText.text = "Distance from sun: " + worldData.distanceFromSunAU + " AU";
-
-        if (worldData.optGroundMaterial != null)
+        // See if the world index already exists (since it was loaded in another scene)
+        var currentWorldIndex = GameObject.Find("CurrentWorldIndex");
+        if (currentWorldIndex == null)
         {
-            groundPlane.material = worldData.optGroundMaterial;
+            // Since it is not there, create it, and mark it so it isn't destroyed when the scene changes
+            currentWorldIndex = Instantiate(currentWorldIndexPrefab);
+            DontDestroyOnLoad(currentWorldIndex);
+        }
+
+        // Get the current world Data by using the world index value, and pulling out the index
+        var worldIndexObj = currentWorldIndex.GetComponent<CurrentWorldIndex>();
+        currentWorldData = worldDataList[worldIndexObj.currentWorldIndex];
+
+        // Update text according to the world we're using
+        titleText.text = "Welcome to " + currentWorldData.worldName;
+
+        gravityText.text = "Gravity: " + currentWorldData.gravityMPS2 + "m/s^2";
+        temperatureText.text = "Temperature: " + currentWorldData.minTempF + "-" + currentWorldData.maxTempF + " F";
+        dayLengthText.text = "Day length: " + currentWorldData.dayNightRotationLength.ToString();
+        distanceFromSunText.text = "Distance from sun: " + currentWorldData.distanceFromSunAU + " AU";
+
+        gravitySetting.GravityValue = currentWorldData.gravityMPS2;
+
+        // Update the ground plane
+        if (currentWorldData.optGroundMaterial != null)
+        {
+            groundPlane.material = currentWorldData.optGroundMaterial;
         }
     }
 
